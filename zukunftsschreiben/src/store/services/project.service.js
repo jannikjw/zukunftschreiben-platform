5607d679f29ac186ccc6ef79d007377b50bd23ca
felanders <felderer.a@outlook.com>
2020-05-15 15:27:02 +0200

    Create a project service with function create

---
+++ zukunftsschreiben/src/store/services/project.service.js
@@ -0,0 +1,47 @@
+import { authHeader } from '../../helpers';
+
+const API_URL = process.env.REACT_APP_API_HOST + '/api'
+
+export const projectService = {
+    create,
+};
+
+// -----------------
+// private functions
+// ----------------- 
+
+// For other responses a 401 error means that the JWT is expired
+// If this is the case, delete the use and perform a hard reload the page
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+
+            if (response.status === 401) {
+                // auto-logout if 401 response returned from api
+                localStorage.removeItem(process.env.REACT_APP_LOCAL_STORAGE_KEY_FOR_USER);
+                window.location.reload(true);
+            }
+
+            const error = data
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
+
+// -----------------
+// service functions
+// -----------------
+function create(title, description, category, status, startDate, endDate) {
+    const requestOptions = {
+        method: 'POST',
+        headers: authHeader(),
+        body: JSON.stringify({ title, description, category, status, startDate, endDate })
+    };
+
+    return fetch(`${API_URL}/project/create`, requestOptions)
+        .then(handleResponse);
+}
+
