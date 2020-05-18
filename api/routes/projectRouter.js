889eda03ba3c8f7a3229ae2631545546e07ad9e1
felanders <felderer.a@outlook.com>
2020-05-18 20:09:05 +0200

    Add get Project to rout

--- api/routes/projectRouter.js
+++ api/routes/projectRouter.js
@@ -4,6 +4,7 @@ const ProjectController = require('../controllers/ProjectController');
 var router = express.Router();
 
 // Authentication Required
-router.post('/', ProjectController.createProject);
+router.post('/create', ProjectController.createProject);
+router.get('/getProject', ProjectController.getProject);
 
 module.exports = router;
