f28e0126378d77562ff87a4922e14848b7847326
felanders <felderer.a@outlook.com>
2020-05-18 20:09:21 +0200

    add get Project Action

--- zukunftsschreiben/src/store/actions/project.actions.js
+++ zukunftsschreiben/src/store/actions/project.actions.js
@@ -4,6 +4,7 @@ import { projectService } from '../services';
 
 export const projectActions = {
   create,
+  getProject,
 };
 
 function create(title, description, category, status, startDate, endDate) {
@@ -24,4 +25,24 @@ function create(title, description, category, status, startDate, endDate) {
   function request(title) { return { type: projectConstants.CREATE_REQUEST_INITIATED, title } }
   function success(project) { return { type: projectConstants.CREATE_REQUEST_SUCCEEDED, project } }
   function failure(error) { return { type: projectConstants.CREATE_REQUEST_FAILED, error } }
+}
+
+function getProject(id) {
+  return dispatch => {
+    dispatch(request({ id }));
+
+    projectService.getProject(id)
+      .then(
+        project => {
+          dispatch(success(project));
+        },
+        error => {
+          dispatch(failure(error));
+        }
+      );
+  };
+
+  function request(id) { return { type: projectConstants.GET_PROJECT_REQUEST_INITIATED, id } }
+  function success(project) { return { type: projectConstants.GET_PROJECT_REQUEST_SUCCEEDED, project } }
+  function failure(error) { return { type: projectConstants.GET_PROJECT_REQUEST_FAILED, error } }
 }
