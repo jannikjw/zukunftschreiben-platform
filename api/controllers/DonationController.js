a46b6b877df2a1d2b1df021dbe47bb3cc608f25c
Lunter <jayjay1110@hotmail.de>
2020-06-05 15:19:08 +0200

    -changed Metadata to Zukunftschreiben - added possibilitz to donate to a project in backend

--- api/controllers/DonationController.js
+++ api/controllers/DonationController.js
@@ -1,9 +1,31 @@
 const DonationModel = require("../models/DonationModel");
+const ProjectModel = require("../models/ProjectModel");
+
 const { body } = require("express-validator");
+const apiResponse = require("../helpers/apiResponse");
 const rejectRequestsWithValidationErrors = require("../middleware/rejectRequestsWithValidationErrors");
 const authenticationRequired = require("../middleware/authenticationRequired");
 const injectProjectFromID = require("../middleware/injectProjectFromID");
 
+
+
+function updateProjectResponse(res, msg, project_id, user_id) {
+  ProjectModel.findById(project_id)
+    .populate('donations')
+    // .exec((err, project) => { if (err) console.log(err); })
+    .then((project) => {
+      console.log(project.toApiRepresentation(user_id))
+      if (!project) {
+        return apiResponse.notFoundResponse(res, "Project not found.");
+      } else {
+        let projectData = project.toApiRepresentation(user_id);
+        return apiResponse.successResponseWithData(res, msg, projectData);
+      }
+    })
+    .catch((err) => {
+      return apiResponse.ErrorResponse(res, "Internal Server Error");
+    });
+}
 
 /**
  * Create a new Donation.
@@ -17,33 +39,26 @@ exports.createDonation = [
   body("amount", "Donation Amount is required.")
     .isDecimal()
     .withMessage("The Donation Amount needs to be a decimal value")
-    .custom(amount => {
-      if (amount < 5) { throw new Error('The Donation Amount needs to be at least 5€') }
-    })
+    // .custom(amount => {
+    //   console.log(parseFloat(amount))
+    //   if (parseFloat(amount) < 5.00) { throw new Error() }
+    // })
+    .withMessage('The Donation Amount needs to be at least 5€')
     .trim(),
   rejectRequestsWithValidationErrors,
   injectProjectFromID,
-  async (req, res) => {
+  (req, res) => {
     try {
       let donation = DonationModel({
         author: req.user._id,
         project: req.project._id,
         amount: Math.round(req.body.amount * 100)
       });
-
-      const savedDonation = await donation.save();
-
-      ProjectModel.findById(savedDonation.project)
-        .populate('donations')
-        .then((project) => {
-          if (!project) {
-            return apiResponse.notFoundResponse(res, "Project not found.");
-          } else {
-            let projectData = project.toApiRepresentation(user_id);
-            return apiResponse.successResponseWithData(res, msg, projectData);
-          }
+      donation.save()
+        .then(savedDonation => {
+          return updateProjectResponse(res, 'Donation of ' + (savedDonation.amount / 100).toFixed(2) + '€ recorded.', savedDonation.project, req.user._id)
         })
-
+        .catch(err => { return apiResponse.ErrorResponse(res, err) })
     } catch (err) {
       return apiResponse.ErrorResponse(res, err);
     }
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.json({ status: 1, message: 'Centerling News API says "Hello World!"' });
+  res.json({ status: 1, message: 'Zukunftschreiben Donation API says "Hello World!"' });
 });
 
 module.exports = router;
