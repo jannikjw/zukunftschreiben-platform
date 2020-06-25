829e9d6c764b4f55f947a7d23b0505cfde8a7eb0
Andreas Felderer <andreas.felderer@tum.de>
2020-06-25 22:52:18 +0200

    fix add project controller

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -13,59 +13,58 @@ mongoose.set("useFindAndModify", false); //false to use native findOneAndUpdate(
 /**
  * Create a new Project.
  *
- * @param {string}      title
- * @param {string}      url
- *
  * @return {Project}}
  */
 exports.createProject = [
   authenticationRequired,
-  body("title", "Title is required.")
-    .isLength({ min: 1, max: 100 })
-    .withMessage("The title needs to be between 1 and 100 characters long.")
-    .trim(),
-  rejectRequestsWithValidationErrors,
-  async (req, res) => {
-    try {
-      let project = ProjectModel({
-        title: req.body.title,
-        description: req.body.description,
+	body("title", "Title is required.")
+		.isLength({ min: 1, max: 100 })
+		.withMessage("The title needs to be between 1 and 100 characters long.")
+		.trim(),
+	(req, res) => {  
+		try {
+			let project = ProjectModel({
+				title: req.body.title,
+				description: req.body.description,
         category: req.body.category,
-        status: req.body.status,
-        startDate: new Date(req.body.startDate),
-        endDate: new Date(req.body.endDate),
-        author: req.user._id,
+        hidden: req.body.hidden,
+				startDate: new Date(req.body.startDate),
+				endDate: new Date(req.body.endDate),
+				author: req.user._id,
+        image: req.body.image,
         username: req.user.username,
-      });
-
-      const savedProject = await project.save();
-
-      let projectData = savedProject.toApiRepresentation(req.user._id);
-      return apiResponse.successResponseWithData(
-        res,
-        "Project successfully created",
-        projectData
-      );
-
-    } catch (err) {
-      return apiResponse.ErrorResponse(res, err);
-    }
-  },
+        fundingGoal: new Number(req.body.fundingGoal),
+        likes: 0,
+			});
+      
+      project.save()
+        .then(response => {
+          return apiResponse.successResponseWithData(
+            res,
+            "Project successfully created",
+            response
+          )
+        })
+        .catch(err => {console.log(err);return apiResponse.ErrorResponse(res, err)})
+		} catch (err) {
+			return apiResponse.ErrorResponse(res, err);
+		}
+	},
 ];
 
 exports.getProject = [
-  authenticationRequired,
-  (req, res) => {
+	authenticationRequired,
+	(req, res) => {
 		try {
-      const query = {_id : req.body.id};
-      ProjectModel.findOne(query, (err, Project) => {
-        if (err) { return apiResponse.ErrorResponse(res, err); }
-        else {
-          return apiResponse.successResponseWithData(res,"Account confirmed.", Project);
-        }
-      });
-    } catch (err) {
+			const query = {_id : req.body.id};
+			ProjectModel.findOne(query, (err, Project) => {
+				if (err) { return apiResponse.ErrorResponse(res, err); }
+				else {
+					return apiResponse.successResponseWithData(res,"Account confirmed.", Project);
+				}
+			});
+		} catch (err) {
 			return apiResponse.ErrorResponse(res, err);
-    }
-  },
+		}
+	},
 ];
