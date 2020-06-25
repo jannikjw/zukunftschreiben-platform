af058a4a8d366f22ad686403c6f01c6dae6a49fb
Andreas Felderer <andreas.felderer@tum.de>
2020-06-25 22:52:49 +0200

    change the project model to the required params

--- api/models/ProjectModel.js
+++ api/models/ProjectModel.js
@@ -2,34 +2,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    category: { type: String, required: true },
-    status: { type: String, required: true, default: 'hidden' },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-    author: { type: String, required: true },
-    username: { type: String, required: true },
-    likes: { type: Number, required: true, default: 0 },
-    comments: { type: [String], required: false }
-  },
-  {
-    timestamps: true,
-    toObject: { virtuals: true },
-    toJSON: { virtuals: true },
-  }
+	{
+		title: { type: String, required: true },
+		description: { type: String, required: true },
+		category: { type: String, required: true },
+		hidden: { type: Boolean, required: true, default: true },
+		startDate: { type: Date, required: true },
+		endDate: { type: Date, required: true },
+		author: { type: String, required: true },
+		image: { type: String, required: true },
+		username: { type: String, required: true },
+		fundingGoal: { type: Number, required: true },
+		likes: { type: Number, required: true, default: 0 },
+		comments: { type: [String], required: false }
+	},
+	{
+		timestamps: true,
+		toObject: { virtuals: true },
+		toJSON: { virtuals: true },
+	}
 );
 
 ProjectSchema.method("toApiRepresentation", function (user) {
-  let obj = this.toObject();
-  obj.author = user._id;
-  obj.username = user.username;
-  return obj;
+	let obj = this.toObject();
+	obj.author = user._id;
+	obj.username = user.username;
+	return obj;
 });
 
 ProjectSchema.method("updateLastInteraction", function () {
-  return;
+	return;
 });
 
 module.exports = mongoose.model("Project", ProjectSchema);
